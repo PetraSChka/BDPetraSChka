f068043377899ca9f67292c28635b26342a4f799
PetraSChka <43903177+PetraSChka@users.noreply.github.com>
2020-05-06 17:02:31 +0300

    Add Task

---
+++ MMF_9_14/install/create_user.sql
@@ -0,0 +1,20 @@
+PROMPT *******************************
+PROMPT Script start: create_user.sql
+PROMPT *******************************
+
+SET SERVEROUTPUT ON
+
+define SCHEMA_NAME = &&1
+define SCHEMA_PASSWORD = &&2
+define DEFAULT_TBS = &&3
+
+CREATE USER &&SCHEMA_NAME
+IDENTIFIED BY &&SCHEMA_PASSWORD
+DEFAULT TABLESPACE &&DEFAULT_TBS
+TEMPORARY TABLESPACE temp
+QUOTA 20M on &&DEFAULT_TBS;
+
+undefine SCHEMA_NAME 
+undefine SCHEMA_PASSWORD
+undefine DEFAULT_TBS
+
---
+++ MMF_9_14/install/main.sql
@@ -0,0 +1,42 @@
+SET TIME ON
+SET TIMING ON
+SET APPINFO ON
+SPOOL INSTALL_SCHEMA.LOG
+
+DEFINE USER_NAME = &&1
+DEFINE TBS_LOCATION =  &&2
+DEFINE SCHEMA_PASSWORD = &&3
+
+SET SERVEROUTPUT ON
+
+PROMPT *********
+PROMPT Username to create: &&USER_NAME
+PROMPT Path of datafile location: &&TBS_LOCATION
+PROMPT *********
+PROMPT
+
+PROMPT *********
+PROMPT Calling create_tablespace.sql for data tablespace
+@create_tablespace.sql &&USER_NAME &&TBS_LOCATION DATA
+PROMPT *********
+PROMPT
+
+PROMPT *********
+PROMPT Calling create_user.sql 
+@create_user.sql &&USER_NAME &&SCHEMA_PASSWORD &&USER_NAME._DATA
+PROMPT *********
+PROMPT
+
+PROMPT *********
+PROMPT Calling grant.sql for &&USER_NAME
+@grants.sql &&USER_NAME
+PROMPT *********
+PROMPT
+
+SET SERVEROUTPUT OFF
+
+UNDEFINE USER_NAME;
+UNDEFINE TBS_LOCATION;
+UNDEFINE SCHEMA_PASSWORD
+
+SPOOL OFF
