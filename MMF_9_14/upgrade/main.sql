abf191417ccff58b21253db4ea625c294284a53f
PetraSChka <43903177+PetraSChka@users.noreply.github.com>
2020-06-04 02:20:31 +0300

    add

--- MMF_9_14/upgrade/main.sql
+++ MMF_9_14/upgrade/main.sql
@@ -1,15 +1,34 @@
-SET TIME ON
 SET TIMING ON
-SPOOL UPGRADE_SCHEMA.LOG
+
+SPOOL UPGRADE001_SCHEMA.LOG
+SET SERVEROUTPUT ON
 
 DEFINE USER_NAME = &&1
 
-SET SERVEROUTPUT ON
-PROMPT Username to upgrade: &&USER_NAME
+PROMPT Connecting to user &&USER_NAME
+
 connect &&USER_NAME/oracle 
 
+PROMPT Creating tables
+
+-- Creating tables
+@upgrade001\create_table_coaches.sql
+@upgrade001\create_table_leagues.sql
+@upgrade001\create_table_matches.sql
+@upgrade001\create_table_player_statistics.sql
+@upgrade001\create_table_players.sql
+@upgrade001\create_table_seasons.sql
+@upgrade001\create_table_stadiums.sql
+@upgrade001\create_table_team_statistics.sql
+@upgrade001\create_table_teams.sql
+@upgrade001\create_table_transfers.sql
+@upgrade001\create_table_events.sql
+
+-- Creating constraints
+PROMPT Creating constraints
+@upgrade001\create_constraints.sql &&USER_NAME
+
 SET SERVEROUTPUT OFF
+SPOOL OFF
 
 UNDEFINE USER_NAME
-
-SPOOL OFF
